33509652311db0617752ef1622523c1f0b529ed1
Iram Nagthan <104900889+z-ira@users.noreply.github.com>
2022-09-12 22:38:57 +0530

    Add files via upload

---
+++ AI Task 4 Set Operations.py
@@ -0,0 +1,12 @@
+E = {0, 2, 4, 6, 8}
+N = {1, 2, 3, 4, 5}
+
+U = E|N #union 
+I = E&N #intersection
+D = E-N #Difference
+S = E^N #Symmetric difference
+
+print("Union of E and N is",U)
+print("Intersection of E and N is",I)
+print("Difference of E and N is",D)
+print("Symmetric difference of E and N is",S)
